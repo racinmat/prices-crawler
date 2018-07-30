a4c9f2ce2f938d50252df8540b6f2f2d7e6dffee
racinmat <racinsky.matej@seznam.cz>
2018-07-30 16:39:20 +0200

    now connection is created and closed for every individual write

--- db.py
+++ db.py
@@ -2,12 +2,13 @@ import sqlite3
 
 
 class Database:
-    db = sqlite3.connect('prices_db')
 
     @staticmethod
     def insert_price(product, price, shop, time):
-        cursor = Database.db.cursor()
+        db = sqlite3.connect('prices_db')
+        cursor = db.cursor()
         cursor.execute('''
         INSERT INTO prices(product, price, shop, time) VALUES (?, ?, ?, ?)
         ''', (product, price, shop, str(time)))
-        Database.db.commit()
+        db.commit()
+        db.close()
